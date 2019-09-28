998370b04610f0ab9571d46942997b4be48181ae
ryo <ba068082@gmail.com>
2019-09-28 15:40:58 +0900

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Hello Project</h1>
+  </div>
+</template>
+
+<script>
+import { createComponent } from "@vue/composition-api";
+
+export default createComponent({
+  setup() {}
+});
+</script>
